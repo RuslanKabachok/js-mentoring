f8c6d3eff329ac712ccc327aa8c72430bd9eed77
RuslanKabachok <ruslan.kabachok@gmail.com>
2025-09-24 20:49:06 +0300

    finished live coding tasks

--- index.js
+++ index.js
@@ -91,3 +91,111 @@ function countLetters(str) {
 }
 
 // console.log(countLetters("Hello World"));
+
+// Task #6
+
+function findAdult(users) {
+    for (let index = 0; index < users.length; index++) {
+        const person = users[index];
+        if (person.age > 18) {
+            return person
+        }
+    }
+}
+
+// function findAdult(users) {
+//     return users.find(user => user.age > 18)
+// }
+
+// console.log(findAdult([{ name: "Ivan", age: 19 }, { name: "Oksana", age: 2 }
+// ]));
+
+// Task #7
+
+// перше до чого додумався
+// function countByCategory(items) {
+//     const result = {};
+
+//     for (let i = 0; i < items.length; i++) {
+//         const element = items[i];
+//         if (result[element.category]) {
+//             result[element.category]++;
+//         } else {
+//             result[element.category] = 1;
+//         }
+//     }
+//     return result;
+// }
+
+// найбільш стислий варіант, до якого сам дійшов
+function countByCategory(items) {
+    const result = {};
+
+    for (const item of items) {
+        result[item.category] = result[item.category] ? result[item.category] + 1 : 1;
+    }
+
+    return result;
+}
+
+//покращений варіант з допомогою ші
+
+// function countByCategory(items) {
+//     return items.reduce((accumulator, item) => {
+//         accumulator[item.category] = (accumulator[item.category] || 0) + 1;
+//         return accumulator;
+//     }, {});
+// }
+
+// console.log(countByCategory([
+//     { name: "Phone", category: "tech" },
+//     { name: "Laptop", category: "tech" },
+//     { name: "Shirt", category: "clothes" }
+// ]));
+
+// Task #8
+
+
+
+function findFactors(number) {
+    const factors = [];
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            factors.push(i)
+        }
+    }
+
+    return factors;
+}
+
+// console.log(findFactors(200)); 
+
+//Task 9
+
+function fib(n) {
+    if (n <= 1) return n;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
+}
+
+// console.log(fib(9)); 
+
+function findPair(arr, sum) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === sum) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+    return null;
+}
+
+// console.log(findPair([1, 2, 3, 4, 5, 6, 7, 8], 8)); 
